237f319ff7ab010fc5501d128e6570a42a8160ee
Nikita Guliaev <gulnikita63@gmail.com>
2020-08-18 14:52:55 -0400

    Updates

---
+++ precache-manifest.ede4d27e1c90a8cb61ebfb255fc12df0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11a43b2b8ba1b77b33a1e2923858b24",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "02d0c59b52458fd62d29",
+    "url": "/pokemon/static/css/main.d77d1c61.chunk.css"
+  },
+  {
+    "revision": "db52a28e9030b77470a4",
+    "url": "/pokemon/static/js/2.8ca9ba3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon/static/js/2.8ca9ba3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d0c59b52458fd62d29",
+    "url": "/pokemon/static/js/main.f3697923.chunk.js"
+  },
+  {
+    "revision": "885091869a4f7b6ff879",
+    "url": "/pokemon/static/js/runtime-main.1a9cb6e4.js"
+  },
+  {
+    "revision": "ed23546e87a1e3a681dc470fda50d004",
+    "url": "/pokemon/static/media/Pokeball.ed23546e.png"
+  },
+  {
+    "revision": "50fa107071e5a7e8d55c387b94872a4c",
+    "url": "/pokemon/static/media/background1.50fa1070.png"
+  },
+  {
+    "revision": "de8a0f82f1449b48f71828ede116ecd9",
+    "url": "/pokemon/static/media/logo.de8a0f82.png"
+  },
+  {
+    "revision": "b23ae5ff80411a06e3bf009f8264f3f6",
+    "url": "/pokemon/static/media/question.b23ae5ff.png"
+  }
+]);
